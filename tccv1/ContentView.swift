f6542e6e25e35c78afaddbbb79cd16adf80b362b
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2024-08-15 21:15:56 -0300

    first commit

---
+++ tccv1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tccv1
+//
+//  Created by thaxz on 15/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tccv1/tccv1App.swift
@@ -0,0 +1,17 @@
+//
+//  tccv1App.swift
+//  tccv1
+//
+//  Created by thaxz on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct tccv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
